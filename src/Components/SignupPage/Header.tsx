95f0699dce32192686f2da03123892e1a51644d5
Daniel Jurma <danieljurma@MacBook-Pro-Daniel.local>
2023-10-13 12:11:33 +0300

    Linked spotify logo to the homepage when clicked on it

--- src/Components/SignupPage/Header.tsx
+++ src/Components/SignupPage/Header.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="header">
-      <img
-        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-        alt=""
-      />
+      <Link to="/" className="Link">
+        <img
+          src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+          alt=""
+        />
+      </Link>
     </div>
   );
 }
